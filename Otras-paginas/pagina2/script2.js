e8a63292de557f5cf14fbe493c5cfd50a0e6debf
stefania <ssantulli@itba.edu.ar>
2023-10-26 21:28:21 -0300

    actualizacion 2
    actualizacion 2

--- Otras-paginas/pagina2/script2.js
+++ Otras-paginas/pagina2/script2.js
@@ -1,5 +1,15 @@
-let nombre=prompt("hello")
+let imgjava = document.getElementsByClassName("imgjava")
+console.log(imgjava)
 
-document.getElementById("titular").innerHTML=
-"chau "+nombre;
+function cambiarfondo(){
+    
+this.style.filter = "blur(10px)"
+}
+function cambiarfondo2(){
+    
+    this.style.filter = "blur(0px)"
+    }
+
+imgjava[0].addEventListener("mouseover",cambiarfondo)
+imgjava[0].addEventListener("mouseleave",cambiarfondo2)
  
