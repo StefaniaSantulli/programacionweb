281e58408128df0cbd7cc6eb6a3dcd33a8caa700
stefania <ssantulli@itba.edu.ar>
2023-11-02 21:14:13 -0300

    clase

--- Otras-paginas/pagina2/script2.js
+++ Otras-paginas/pagina2/script2.js
@@ -6,10 +6,18 @@ function cambiarfondo(){
 this.style.filter = "blur(10px)"
 }
 function cambiarfondo2(){
-    
     this.style.filter = "blur(0px)"
+
     }
 
-imgjava[0].addEventListener("mouseover",cambiarfondo)
-imgjava[0].addEventListener("mouseleave",cambiarfondo2)
- 
+function ponerId(indice){
+    codigo='00'
+    codigoFinal=codigo+indice
+    console.log('Identificador: ' + codigoFinal);
+}
+
+for(let i=0;i<=imgjava.length-1;i++){
+    imgjava[i].addEventListener("mouseover",cambiarfondo,)
+    imgjava[i].addEventListener("mouseover",ponerId)
+    imgjava[i].addEventListener("mouseleave",cambiarfondo2) 
+}
