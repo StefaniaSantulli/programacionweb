2c2c9fd169c18f2f36a9e112bc49c961612d5cd9
stefania <ssantulli@itba.edu.ar>
2023-11-09 21:17:35 -0300

    jueves

--- Otras-paginas/pagina2/script2.js
+++ Otras-paginas/pagina2/script2.js
@@ -1,23 +1,48 @@
 let imgjava = document.getElementsByClassName("imgjava")
 console.log(imgjava)
 
+let numeroDeImagen = 0;
+
 function cambiarfondo(){
     
-this.style.filter = "blur(10px)"
 }
 function cambiarfondo2(){
     this.style.filter = "blur(0px)"
+}
 
-    }
-
-function ponerId(indice){
-    codigo='00'
-    codigoFinal=codigo+indice
+function ponerId(i){
+    codigo='00';
+    codigoFinal=codigo+i;
     console.log('Identificador: ' + codigoFinal);
+    console.log(i);
+    let content = document.getElementById(i);
+    console.log(content);
+    content.innerHTML = 'sdkfkdg';
+}
+
+
+for(let i=0;i<=imgjava.length-1;i++){
+    imgjava[i].addEventListener("mouseleave", cambiarfondo2);
 }
 
 for(let i=0;i<=imgjava.length-1;i++){
-    imgjava[i].addEventListener("mouseover",cambiarfondo,)
-    imgjava[i].addEventListener("mouseover",ponerId)
-    imgjava[i].addEventListener("mouseleave",cambiarfondo2) 
+    numeroDeImagen = i;
+    imgjava[i].addEventListener("mouseover", function (e) {
+        this.style.filter = "blur(10px)";
+        // ponerId(numeroDeImagen); // numeroDeImagen (variable global) toma el valor de i para no usar parentesis en cambiarfondo
+        let imagenActual = this;
+        console.log(imagenActual.attr('id'));
+
+        // while (articleNode.nodeName != "ARTICLE" && articleNode.nodeName != "BODY") {
+        //   articleNode = articleNode.parentNode;
+        // }
+        // if (articleNode.nodeName == "BODY") {
+        //   // no article ancestor was found
+        // } else {
+        //   // do something with articleNode
+        //   console.log(articleNode.id);
+        // }
+
+        e.stopPropagation();
+      });
 }
